d525ad62b3fa24c54821073b8139694d1f5d761e
Serkhani <deviper03@gmail.com>
2023-06-27 13:28:32 +0000

    added app sizes

---
+++ lib/resources/sizes.dart
@@ -0,0 +1,53 @@
+class Sizes {
+  Sizes._();
+
+  // margins and spacing
+  static const marginExtraSmall = 4.0;
+  static const marginSmall = 8.0;
+  static const marginMedium = 12.0;
+  static const marginNormal = 16.0;
+  static const marginLarge = 20.0;
+  static const marginExtraLarge = 24.0;
+
+  static const spacingSmall = 28.0;
+  static const spacingNormal = 32.0;
+  static const spacingLarge = 36.0;
+  static const spacingExtraLarge = 45.0;
+
+  // radii
+  static const radiusMini = 8.0;
+  static const radiusExtraSmall = 12.0;
+  static const radiusSmall = 18.0;
+  static const radiusNormal = 24.0;
+  static const radiusLarge = 32.0;
+  static const radiusBottomSheet = 45.0;
+
+  // font sizes
+  static const fontSizeSmallDescription = 10.0;
+  static const fontSizeDescription = 12.0;
+  static const fontSizeNormal = 14.0;
+  static const fontSizeTitle = 16.0;
+  static const fontSizeLargeTitle = 18.0;
+  static const fontSizeLargeTitleAlt = 21.0;
+
+  // home banner dimensions
+  static const fontSizeHomeBannerAppName = 45.0;
+  static const bannerTopSpacing = 20.0;
+  static const stackOffset = 27;
+
+  // icon sizes
+  static const iconSizeExtraSmall = 16.0;
+  static const iconSizeSmall = 18.0;
+  static const iconSizeNormal = 24.0;
+
+  // widget heights and widths
+  static const primaryButtonHeight = 55.0;
+  static const homeAppBarHeight = 70.0;
+  static const homeWelcomeBannerHeight = 250.0;
+  static const peerAdvisorConnectBannerHeight = 220.0;
+  static const homeDoctorsBannerHeight = 120.0;
+  static const doctorsSectionCarouselHeight = 350.0;
+  static const servicesCardHeight = 250.0;
+  static const servicesCardWidth = 220.0;
+
+}
