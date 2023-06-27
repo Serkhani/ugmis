bb6d83aef92c3505c7dbc71322ceeac6225e8558
Serkhani <deviper03@gmail.com>
2023-06-27 06:27:15 +0000

    added app strings

---
+++ lib/resources/app_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class AppStrings {
+  AppStrings._();
+
+  // General
+  static const ses = 'SCHOOL OF ENGINEERING SCIENCES';
+
+  // Login
+  static const welcome = 'Welcome back! Glad to see you , Again!';
+  static const enterYourId = ' Enter your Id';
+  static const enterYourIdEg = '109*****';
+  static const enterYourPin = ' Enter your Pin';
+  static const enterYourPinEg = '12345';
+  static const logIn = "Log In";
+  static const noAccount = "Don't have an account?";
+  static const registerNow = "Register Now";
+}
