b72ae430ada015ae95cff1233549a38d571d0ce4
Serkhani <deviper03@gmail.com>
2023-06-27 07:49:17 +0000

    added animations

--- lib/app/modules/launcher/controllers/launcher_controller.dart
+++ lib/app/modules/launcher/controllers/launcher_controller.dart
@@ -1,23 +1,64 @@
+import 'dart:async';
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 
-class LauncherController extends GetxController {
-  //TODO: Implement LauncherController
+import '../../../../app/routes/app_pages.dart';
+
+class LauncherController extends GetxController
+    with GetTickerProviderStateMixin {
+  AnimationController? scaleController;
+  AnimationController? progressController;
+  Animation<double>? scaleAnimation;
+  Animation<double>? progressAnimation;
 
   final count = 0.obs;
   @override
   void onInit() {
+    progressController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    scaleController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 3),
+        reverseDuration: const Duration(seconds: 1));
+    scaleAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(scaleController!);
+    progressAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(progressController!);
+
+    scaleController!.addStatusListener((status) {
+      if (status == AnimationStatus.dismissed) {
+        Timer(
+          const Duration(seconds: 1),
+          (() => Get.offNamed(Routes.HOME)),
+        );
+      }
+    });
+
+    scaleController!.addListener(() {
+      if (scaleController!.isAnimating) {
+        update(['animation']);
+      }
+    });
+
     super.onInit();
   }
 
   @override
   void onReady() {
+    Timer(
+      const Duration(seconds: 1),
+      () {
+        scaleController!.forward();
+        progressController!.forward();
+      },
+    );
     super.onReady();
   }
 
   @override
   void onClose() {
+    scaleController!.dispose();
+    progressController!.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
