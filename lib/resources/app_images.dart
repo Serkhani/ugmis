949d1fb4f6e101caf51d8bd2f7ed19477e533edc
Serkhani <deviper03@gmail.com>
2023-06-28 14:04:07 +0000

    added dashboard images

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -3,5 +3,11 @@ class AppImages {
 
   // pngs
   static const sesLogo = 'assets/images/ses_logo.png';
+  static const announcement = 'assets/images/announcement.png';
+  static const clubs = 'assets/images/clubs.png';
+  static const pastQuestions = 'assets/images/past_questions.png';
+  static const slides = 'assets/images/slides.png';
+  static const tasks = 'assets/images/tasks.png';
+  static const timetable = 'assets/images/timetable.png';
   
 }
