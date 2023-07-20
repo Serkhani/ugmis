929c3d7809f5bbb67ff405931405a21c6ba404f6
Serkhani <deviper03@gmail.com>
2023-07-20 00:36:35 +0000

    edited dashboard

--- lib/app/modules/dashboard/views/dashboard_tabview.dart
+++ lib/app/modules/dashboard/views/dashboard_tabview.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ugmis/resources/app_colors.dart';
+import 'package:ugmis/resources/sizes.dart';
 import '../controllers/dashboard_controller.dart';
 
 class DashboardViewTab extends GetView<DashboardController> {
@@ -10,14 +12,26 @@ class DashboardViewTab extends GetView<DashboardController> {
     return GridView.builder(
       itemCount: controller.images.length,
       itemBuilder: (context, index) {
-        return Card(
-          elevation: 4.0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(controller.images[index]),
-              Text(controller.strings[index])
-            ],
+        return Container(
+          width: 150.0,
+          height: 150.0,
+          padding: const EdgeInsets.all(8.0),
+          child: PhysicalModel(
+            clipBehavior: Clip.hardEdge,
+            borderRadius: BorderRadius.circular(Sizes.radiusSmall),
+            elevation: 4.0,
+            color: AppColors.primary,            
+            child: Column(
+              children: [
+                Expanded(child: Image.asset(controller.images[index],)),
+                Container(
+                  alignment: Alignment.center,
+                  height: 30.0,
+                  width: double.infinity,
+                  color: AppColors.white,
+                  child: Text(controller.strings[index])),
+              ],
+            )
           ),
         );
       }, gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
