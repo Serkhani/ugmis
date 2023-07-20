b8232586cde8ac311b38a5c524642db08fe8d35d
Serkhani <deviper03@gmail.com>
2023-07-20 10:05:05 +0000

    added systems list tiles

--- lib/app/modules/dashboard/views/announcement_tabview.dart
+++ lib/app/modules/dashboard/views/announcement_tabview.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ugmis/resources/app_colors.dart';
 
 
-class UpcomingEventsView extends StatelessWidget {
-  const UpcomingEventsView({Key? key}) : super(key: key);
+class SystemsView extends StatelessWidget {
+  const SystemsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No Events'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: ["MIS", "STS", "SAKAI"].map((e) => Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: ListTile(
+            tileColor: AppColors.greyLight,
+            title: Text(e),
+            trailing: const Icon(Icons.arrow_forward_sharp),
+          ),
+        )).toList(),
+      )
     );
   }
 }
