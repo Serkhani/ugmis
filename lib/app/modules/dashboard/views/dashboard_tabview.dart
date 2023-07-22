74b32c9ce00695e5bb9c8d9b394b95fa21d35a69
Serkhani <deviper03@gmail.com>
2023-07-22 14:22:18 +0000

    finished ui for dashboard

--- lib/app/modules/dashboard/views/dashboard_tabview.dart
+++ lib/app/modules/dashboard/views/dashboard_tabview.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:ugmis/app/modules/dashboard/views/models/newstile.dart';
 import 'package:ugmis/resources/app_colors.dart';
 import 'package:ugmis/resources/sizes.dart';
+import '../../../../resources/app_styles.dart';
 import '../controllers/dashboard_controller.dart';
 
 class DashboardViewTab extends GetView<DashboardController> {
@@ -10,43 +11,66 @@ class DashboardViewTab extends GetView<DashboardController> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.lightBlue,
-          height: 200.0,
-          child: const ImageCarousel()
-        ),
-        Expanded(
-          child: GridView.builder(
-            itemCount: controller.images.length,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 150.0,
-                height: 150.0,
-                padding: const EdgeInsets.all(8.0),
-                child: PhysicalModel(
-                  clipBehavior: Clip.hardEdge,
-                  borderRadius: BorderRadius.circular(Sizes.radiusSmall),
-                  elevation: 4.0,
-                  color: AppColors.primary,            
-                  child: Column(
-                    children: [
-                      Expanded(child: Image.asset(controller.images[index],)),
-                      Container(
-                        alignment: Alignment.center,
-                        height: 30.0,
-                        width: double.infinity,
-                        color: AppColors.white,
-                        child: Text(controller.strings[index])),
-                    ],
-                  )
-                ),
-              );
-            }, gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 2.0,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        toolbarHeight: 120.0,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Welcome!\n\t\t\tDoluDolu",
+              style: AppStyles.inputTextLabelStyle.copyWith(
+                fontSize: 30,
+              ),
+            ),
           ),
         ),
-      ],
+      ),
+      body: Column(
+        children: [
+          Container(
+              color: Colors.lightBlue,
+              height: 200.0,
+              child: const ImageCarousel()),
+          Expanded(
+            child: GridView.builder(
+              itemCount: controller.images.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 150.0,
+                  height: 150.0,
+                  padding: const EdgeInsets.all(8.0),
+                  child: PhysicalModel(
+                      clipBehavior: Clip.hardEdge,
+                      borderRadius: BorderRadius.circular(Sizes.radiusSmall),
+                      elevation: 4.0,
+                      color: AppColors.primary,
+                      child: Column(
+                        children: [
+                          Expanded(
+                              child: Image.asset(
+                            controller.images[index],
+                          )),
+                          Container(
+                              alignment: Alignment.center,
+                              height: 30.0,
+                              width: double.infinity,
+                              color: AppColors.white,
+                              child: Text(controller.strings[index])),
+                        ],
+                      )),
+                );
+              },
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ugmis/app/modules/dashboard/views/dashboard_tabview.dart';
 import 'package:ugmis/app/modules/dashboard/views/profile.dart';
-import 'package:ugmis/resources/app_colors.dart';
 import 'package:ugmis/resources/app_strings.dart';
 import 'package:ugmis/resources/app_styles.dart';
 
@@ -16,32 +15,9 @@ class DashboardView extends GetView<DashboardController> {
     return SafeArea(
       child: DefaultTabController(
         initialIndex: 0,
-        length: 2,
+        length: 3,
         child: Scaffold(
-          // appBar: AppBar(
-          //   backgroundColor: Colors.transparent,
-          //   elevation: 0.0,
-          //   actions: [
-          //     Container(
-          //       decoration: BoxDecoration(
-          //         shape: BoxShape.circle,
-          //         border: Border.all(
-          //             width: 3.0,
-          //             strokeAlign: BorderSide.strokeAlignCenter,
-          //             color: Colors.blue),
-          //       ),
-          //       child: IconButton(
-          //         onPressed: () {
-          //           controller.showProfile();
-          //         },
-          //         icon: const Icon(
-          //           Icons.person,
-          //           color: Colors.blue,
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // ),
+          
           body: const TabBarView(
             children: [
               DashboardViewTab(),
