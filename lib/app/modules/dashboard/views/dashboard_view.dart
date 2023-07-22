5d4218715aa192d421c9ec84c9c98f21ee082c7c
Serkhani <deviper03@gmail.com>
2023-07-22 13:25:23 +0000

    removed appbar and added tab

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ugmis/app/modules/dashboard/views/dashboard_tabview.dart';
+import 'package:ugmis/app/modules/dashboard/views/profile.dart';
+import 'package:ugmis/resources/app_colors.dart';
 import 'package:ugmis/resources/app_strings.dart';
 import 'package:ugmis/resources/app_styles.dart';
 
@@ -16,50 +18,70 @@ class DashboardView extends GetView<DashboardController> {
         initialIndex: 0,
         length: 2,
         child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            actions: [
-              Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      width: 3.0,
-                      strokeAlign: BorderSide.strokeAlignCenter,
-                      color: Colors.blue),
-                ),
-                child: IconButton(
-                  onPressed: () {
-                    controller.showProfile();
-                  },
-                  icon: const Icon(
-                    Icons.person,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-            ],
-          ),
+          // appBar: AppBar(
+          //   backgroundColor: Colors.transparent,
+          //   elevation: 0.0,
+          //   actions: [
+          //     Container(
+          //       decoration: BoxDecoration(
+          //         shape: BoxShape.circle,
+          //         border: Border.all(
+          //             width: 3.0,
+          //             strokeAlign: BorderSide.strokeAlignCenter,
+          //             color: Colors.blue),
+          //       ),
+          //       child: IconButton(
+          //         onPressed: () {
+          //           controller.showProfile();
+          //         },
+          //         icon: const Icon(
+          //           Icons.person,
+          //           color: Colors.blue,
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
           body: const TabBarView(
             children: [
               DashboardViewTab(),
               SystemsView(),
+              Profile()              
             ],
           ),
-          bottomNavigationBar: const TabBar(
+          bottomNavigationBar: TabBar(
+            // padding: EdgeInsets.all(value),
+            padding: const EdgeInsets.all(0),
+            automaticIndicatorColorAdjustment: true,
+            
             tabs: [
               Tab(
-                child: Text(
+                iconMargin: const EdgeInsets.all(2.0),
+                icon: Icon(Icons.dashboard_outlined,
+                 color: AppStyles.inputTextLabelStyle.color,),
+                child: const Text(
                   AppStrings.dashboard,
                   style: AppStyles.inputTextLabelStyle,
                 ),
               ),
               Tab(
-                child: Text(
-                  AppStrings.upcomingEvents,
+                iconMargin: const EdgeInsets.all(2.0),
+                icon: Icon(Icons.settings_system_daydream_outlined, 
+                color: AppStyles.inputTextLabelStyle.color,),
+                child: const Text(
+                  AppStrings.systems,
                   style: AppStyles.inputTextLabelStyle,
                 ),
               ),
+              Tab(
+                iconMargin: const EdgeInsets.all(2.0),
+                icon: Icon(Icons.person, 
+                color: AppStyles.inputTextLabelStyle.color,),
+                child: const Text(
+                  AppStrings.profile,
+                  style: AppStyles.inputTextLabelStyle,
+                ),                
+              )
             ],
           ),
         ),
